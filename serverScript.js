d3af210bf51451c58150ef3d7d6c8f25d2b8de26
flockoloco <art913@hotmail.com>
2020-04-23 12:31:20 +0100

    First Commit

---
+++ serverScript.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static("libraries"));
+app.use(express.static("public"));
+app.use(express.static("addons"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
