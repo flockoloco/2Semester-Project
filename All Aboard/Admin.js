bc1cac394201ba2acfb55104fea0c8e8a1717419
flockoloco <art913@hotmail.com>
2020-04-26 15:28:13 +0100

    WebSite

--- All Aboard/Admin.js
+++ All Aboard/Admin.js
@@ -1,8 +1,15 @@
+const path =  require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+router.get('/Credits', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'View', 'Credits.html'));
+});
+
 router.get('/Register', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'View', 'Register.html'));
 });
 
 module.exports = router;
