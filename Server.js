98a2939c1aa4ab75c9173a9c87d2591c563ee151
flockoloco <art913@hotmail.com>
2020-04-29 22:53:16 +0100

    database

--- Server.js
+++ Server.js
@@ -1,22 +1,15 @@
+//import packages
 const path = require('path');
-
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
+//import folders
 const adminR = require('./AllAboard/Admin');
+const route = require('./database/database');
 
-const mysql = require('mysql');
-
-//CODE FOR CONNECTION TO DATABASE
-const dbase = mysql.createConnection({
-    host:"localhost",
-    port:"8889",
-    user:"root",
-    password:"Project100",
-    database:"All_Aboard",
-    });
+const port = 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -33,4 +26,4 @@ app.use(adminR.routes);
 
 app.use((req, res, next) => {res.status(404).sendFile(path.join(__dirname, 'view', '404.html'))});
 
-app.listen(3000);
+app.listen(port);
