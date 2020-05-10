224a8d07a43cc05d557634644e94d08073f29ffe
flockoloco <art913@hotmail.com>
2020-05-10 15:44:05 +0100

    cm

--- ServerSide/register.js
+++ ServerSide/register.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.get('/register', function(req, res) {
     res.sendFile(path.join(__dirname, '..', 'view', 'register.html'));
+    
 });
 
 app.post('/register', function(req, res) {
@@ -13,18 +14,15 @@ app.post('/register', function(req, res) {
 	const username = req.body.username;
     const password = req.body.password;
     const Repeat_password = req.body.Repeat_password;
-
-    connection.query("SELECT * FROM player WHERE username = '"+username+"' ORDER BY username LIMIT 1", function (error, results, fields) {
-        if (results.length > 0) {
-        res.send('The username already exist');
-        }else{
+    
+	if(Repeat_password == password)	{
         connection.query('INSERT INTO player(username,password) VALUES (?, ?)', [username, password], function(error, results, fields) {
         res.redirect('/');
         });
-        if(Repeat_password != password){
-            res.send('The password dont match!');
-        }
-    }});
+    };
+    if(Repeat_password != password){
+        res.send('The password dont match!');
+    }
 });
 
 module.exports = app;
